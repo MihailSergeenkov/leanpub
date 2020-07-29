ca610f2c31bf0526c2a5e946cc34adcd37aec483
Сергеенков Михаил Сергеевич <sergeenkov.mihail@gmail.com>
2020-07-29 09:21:53 +0300

    Второе занятие

--- src/App.js
+++ src/App.js
@@ -2,13 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import BookCard from './BookCard';
 
+const styles = {
+  header: {
+    backgroundColor: '#222222',
+    color: '#fff',
+    minHeight: '50px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: '1.2em',
+    fontWeight: 'bold',
+  },
+  main: {
+    padding: '10px 10%',
+  },
+  footer: {
+    padding: '0 10%',
+    marginTop: '50px',
+  },
+};
+
 class App extends React.Component {
   render() {
     const { book } = this.props;
     return (
-      <div>
-        <BookCard book={book} />
-      </div>
+      <>
+        <header style={styles.header}>
+          Leanpub
+        </header>
+        <main style={styles.main}>
+          <BookCard book={book} />
+        </main>
+        <footer style={styles.footer}>
+          &copy; {new Date().getFullYear()}
+        </footer>
+      </>
     );
   }
 }
