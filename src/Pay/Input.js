86fef97a50227c3a04039f1c2324af69f05c9087
Сергеенков Михаил Сергеевич <sergeenkov.mihail@gmail.com>
2020-08-11 09:37:53 +0300

    Основное задание

--- src/Pay/Input.js
+++ src/Pay/Input.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
-const Input = ({ minimumPrice, currentPay, handleChange }) => (
-  <input 
-    type='number' 
-    min={minimumPrice} 
-    value={currentPay}
-    onChange={handleChange}
-  />
-);
+const Input = ({ minimumPrice, currentPay, handleChange }) => {
+  const refInput = useRef(null);
+
+  useEffect(() => {
+    refInput.current.focus();
+  }, []);
+
+  return (
+    <input 
+      type='number' 
+      min={minimumPrice} 
+      value={currentPay}
+      onChange={handleChange}
+      ref={refInput}
+    />
+  );
+};
 
 Input.propTypes = {
   minimumPrice: PropTypes.number,
